00b64496089f66def276a44519bfa8681f25210d
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-04-14 11:02:28 -0400

    Display and store number on screen

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
+const screen = document.getElementById('screen');
+const numbers = document.querySelectorAll('[data-number]');
+const clearAll = document.getElementById('clear-all');
+const clearLastEntry = document.getElementById('clear-last');
+
+
+let numberCount = 0;
+let enteredNumber = 0;
+
+// Add two numbers
 function add(a, b) {
   console.log(a+b);
   return a+b;
 }
 
+// Subtract two numbers
 function subtract(a, b) {
   console.log(a-b);
   return a-b;
 }
 
+// Multiply two numbers
 function multiply(a, b) {
   console.log(a*b);
   return a*b;
 }
 
+// Divide two numbers
 function divide(a, b) {
   console.log(a/b);
   return a/b;
 }
 
+// Calls operator function on two numbers
 function operate(a, b, operator) {
   switch(operator) {
     case '+':
@@ -34,3 +48,20 @@ function operate(a, b, operator) {
       break; 
   } 
 }
+
+// Display numbers on the screen
+function displayNumber() {
+  const number = this.dataset.number;
+
+  if (numberCount <= 8) {
+    screen.innerHTML+= number;
+    numberCount++;
+    enteredNumber = parseInt(screen.innerHTML);
+  }
+
+  console.log(`Entered number ${enteredNumber}`);
+  return enteredNumber;
+}
+
+// Event Listeners 
+numbers.forEach(number => number.addEventListener('click', displayNumber))
